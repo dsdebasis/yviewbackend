0bc55f32f15b05eed394ba6144945638ee90b3f5
Debasis Sahoo <dsdebasis2@gamil.com>
2024-05-02 16:53:47 +0530

    get channel controller updated for no channel

--- controllers/getChannel.js
+++ controllers/getChannel.js
@@ -7,13 +7,15 @@ import { User } from "../models/user.model.js"
 
 const getChannel = asyncHandler(async(req,res,next)=>{
 
-  const userChannelDetails =await User.findById(req.user._id).populate("channel").select("channel")
+  let userChannelDetails =await User.findById(req.user._id)
  
-  if(userChannelDetails !== undefined){
+  if(userChannelDetails.channel !== undefined ){
     
+    userChannelDetails =await userChannelDetails.populate("channel")
+    userChannelDetails = userChannelDetails.channel
     return res.status(200).json(new ApiResponse(200,"successfully fetched channel details",userChannelDetails))
   } else{
-    throw new ApiError(400,"no channel found")
+    throw new ApiError(400,"no channel found. Create a channel")
   }
   
 })
